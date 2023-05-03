b40cf63bda28acc0407e7ffef909b9dbcec8b189
Dmytro Karpenko <kardmitriy@gmail.com>
2023-05-03 17:19:07 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,68 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 import { Container } from './App.styled';
+import { fetchPhoto } from '../api/fetch-photo';
+import Searchbar from './Searchbar/Searchbar';
 // import { nanoid } from 'nanoid';
 
 // import PropTypes from 'prop-types';
 
 class App extends Component {
-  BASE_URL = 'https://pixabay.com/api/';
-  API_KEY = '34271519-257a556d5fe8c31a240fa9516';
-
-  value = 'cat';
-  numberPage = 1;
-
-  config = {
-    params: {
-      key: this.API_KEY,
-      q: this.value,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      page: this.numberPage,
-      per_page: 12,
-    },
-  };
-
   state = {
-    gallary: null,
+    page: 1,
+    query: '',
+    photos: [],
+    totalItems: 0,
+    loading: false,
   };
 
-  componentDidMount() {
-    const { BASE_URL, config } = this;
-    axios.get(BASE_URL, config).then(gallary => this.setState({ gallary }));
+  // componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   fetchPhoto(this.state.query, this.state.page)
+  //     .then(res => {
+  //       this.setState(() => {
+  //         return {
+  //           photos: [...this.state.photos, ...res.hits],
+  //           totalItems: res.total,
+  //         };
+  //       });
+  //     })
+  //     .catch(error => console.log('Error'))
+  //     .finally(() => this.setState({ loading: false }));
+  // }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevState.query);
   }
+
+  handleFormSubmit = query => {
+    console.log(query);
+    this.setState({ query });
+  };
 
   render() {
     return (
-      <>
+      <div>
         <Container>
           <h2>SearchGallery</h2>
+          <Searchbar onSubmit={this.handleFormSubmit} />
         </Container>
 
-        {this.state.gallary && <h2>Gallery</h2>}
-      </>
+        {this.state.loading && <h1>loading...</h1>}
+        {this.state.photos && <h2>Gallery</h2>}
+
+        <ul>
+          {this.state.photos.map(({ webformatURL, tags }, index) => {
+            return (
+              <li key={index} className="gallery-item">
+                <img src={webformatURL} alt={tags} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     );
   }
 }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,12 +1,47 @@
 import { Component } from 'react';
 
 class Searchbar extends Component {
-  constructor(props) {
-    super(props);
-  }
-  state = {};
+  state = {
+    inputValue: '',
+  };
+
+  handleChange = evt => {
+    this.setState({ inputValue: evt.currentTarget.value.toLowerCase().trim() });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+
+    if (this.state.inputValue.trim() === '') {
+      alert('Зробіть запит');
+      return;
+    }
+    this.props.onSubmit(this.state.inputValue.toLowerCase().trim());
+    this.setState({ inputValue: '' });
+  };
+
   render() {
-    return;
+    const { inputValue } = this.state;
+    return (
+      <header className="searchbar">
+        <form className="form" onSubmit={this.handleSubmit}>
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
+
+          <input
+            className="input"
+            type="text"
+            name="search"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={inputValue}
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
   }
 }
 
